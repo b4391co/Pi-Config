d86e19a2660fc9d88f430633801071cc0f1a5a41
b4391co <119635294+b4391co@users.noreply.github.com>
2023-01-23 23:14:22 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,7 +5,12 @@ dir=$pwd
 echo "[ APT ]"
 
 sudo apt update 
-sudo apt install apache2 apache2-utils php neovim acl hugo  -y
+sudo apt install apache2 apache2-utils php neovim acl hugo snapd docker -y
+sudo apt dist-upgrade
+
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
+docker pull klakegg/hugo
 
 echo "[ APT OK ]"
 
@@ -52,15 +57,22 @@ sudo hugo new site General
 
 cd General
 sudo git init
-sudo git submodule add https://github.com/alex-shpak/hugo-book themes/book
+sudo git submodule add https://github.com/alex-shpak/hugo-book themes/hugo-book
+docker pull klakegg/hugo
 sudo chmod a+w config.toml
 echo "theme = 'book'" >> config.toml
 
 cd $pwd
 sudo cp ./index.md /var/www/html/General/content/_index.md
 cp ./hugo.sh ~
+chmod a+x /home/b4391co/hugo.sh
 
+sudo chown -R html /var/www/html
+sudo chgrp -R usuarios /var/www/html
 
+sudo cp ./rc.local /etc/rc.local
+
+sudo cp ./000-default.conf /etc/apache2/sites-enabled
 echo "PASSWORDS FOR:"
 Echo "Public"
 sudo htpasswd -c /etc/apache2/.htpasswd Public
@@ -69,7 +81,7 @@ sudo htpasswd /etc/apache2/.htpasswd b4391co
 Echo "b4391co"
 sudo htpasswd -c /etc/apache2/.htpasswdPrivate b4391co
 
-sudo service apache2 restart
+sudo systemctl restart apache2 
 
 echo "[ HTML OK ]"
 
