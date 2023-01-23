5858594bd8668e078c29958a6a129193eb31c59c
b4391co <119635294+b4391co@users.noreply.github.com>
2023-01-23 21:26:00 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -21,32 +21,45 @@ echo "[ DUCK OK ]"
 
 echo "[ SSH ]"
 
-adduser -d /var/www/html html
-cp ./html /var/www -R 
-chown -R html /var/www/html
+echo "PASSWORD FOR HTML USER"
+sudo adduser html --home /var/www/html
+
+sudo groupadd usuarios
+sudo usermod -a -G usuarios html
+sudo usermod -a -G usuarios b4391co
+
+sudo cp ./html /var/www -R 
+sudo chmod g+w /var/www/html -R
+
+sudo chown -R html /var/www/html
+sudo chgrp -R usuarios /var/www/html
 
 sudo cp ./sshd_config_2019 /etc/ssh/
+sudo /usr/sbin/sshd -f sshd_config_2019
 
-setfacl -m user:html:--- /usr/bin/su
-setfacl -m user:html:--- /usr/bin/sudo
+sudo setfacl -m user:html:--- /usr/bin/su
+sudo setfacl -m user:html:--- /usr/bin/sudo
 
-sudo service ssh restart
+sudo systemctl restart ssh 
 
 echo "[ SSH OK ]"
 
 echo "[ HTML ]"
 
+sudo rm /var/www/html/index.html
 cd /var/www/html 
 sudo hugo new site General
 
 cd General
-git init
-git submodule add https://github.com/alex-shpak/hugo-book themes/book
+sudo git init
+sudo git submodule add https://github.com/alex-shpak/hugo-book themes/book
+sudo chmod a+w config.toml
 echo "theme = 'book'" >> config.toml
 
 cd $pwd
-cp ./index.md /var/www/html/General/content/_index.md
+sudo cp ./index.md /var/www/html/General/content/_index.md
 cp ./hugo.sh ~
+
 
 echo "PASSWORDS FOR:"
 Echo "Public"
