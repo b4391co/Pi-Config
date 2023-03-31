3acb8c6405e7a739cec2ac09875b35c4f2e507e4
b4391co <b4391co@proton.me>
2023-03-31 20:13:12 +0200

    v0.1.0

--- launch.sh
+++ launch.sh
@@ -5,23 +5,23 @@ username=`whoami`
 echo "Interface:"
 read interface
 
-ip="/usr/sbin/ifconfig $interface | grep "inet " | awk '{print $2}'"
+ip=`/usr/sbin/ifconfig $interface | grep "inet " | awk '{print $2}'`
 
 dir=$pwd
 home=/home/$username
 
 echo "[ APT ]"
 
-echo "deb http://deb.debian.org/debian buster main" >> /etc/apt/sources.list
+sudo echo "deb http://deb.debian.org/debian buster main" >> /etc/apt/sources.list
 sudo apt -y update 
 sudo apt -y install apache2 apache2-utils php neovim acl hugo snapd docker unzip 
 sudo apt-get install -y libapache2-mod-geoip
-a2enmod geoip
+sudo a2enmod geoip
 sudo apt -y dist-upgrade
 
 curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
-docker pull klakegg/hugo
+sudo docker pull klakegg/hugo
 
 echo "[ APT OK ]"
 
@@ -67,12 +67,12 @@ echo "[ HTML ]"
 
 sudo rm /var/www/html/index.html
 cd /var/www/html
-mkdir files
-mkdir filesPrivate
+sudo mkdir files
+sudo mkdir filesPrivate
 hugo new site hugo
 cd hugo
-git init
-git submodule add https://github.com/alex-shpak/hugo-book themes/hugo-book
+sudo git init
+sudo git submodule add https://github.com/alex-shpak/hugo-book themes/hugo-book
 echo "theme = 'hugo-book'" >> config.toml
 
 cd $pwd
@@ -82,6 +82,7 @@ sudo chgrp -R usuarios /var/www/html
 
 sudo cp ./rc.local /etc/rc.local
 sudo cp ./crontab /var/spool/cron/crontabs/$username
+sudo cp ./crontab /var/spool/cron/crontabs/root
 
 sudo cp ./000-default.conf /etc/apache2/sites-enabled
 echo "PASSWORDS FOR:"
@@ -97,13 +98,20 @@ sudo echo "User html" >> /etc/apache2/apache2.conf
 sudo systemctl restart apache2 
 sed -i "s/!!!USER!!!/$username/g" /etc/rc.local
 sed -i "s/!!!USER!!!/$username/g" /var/spool/cron/crontabs/$username
+sed -i "s/!!!USER!!!/$username/g" /var/spool/cron/crontabs/root
 sed -i "s/!!!USER!!!/$username/g" ~/hugo.sh
+
+sudo cp -r ./apache2 /etc/ 
+sudo systemctl restart apache2 
 
 echo "[ HTML OK ]"
 
 echo "[ PIVPN ]"
 
 curl -L https://install.pivpn.io | bash
+
+ipvpn=`ip addr | grep '10\.' | awk '{print $2}'`
+sudo iptables -t nat -A POSTROUTING -s $ipvpn -o $interface -j MASQUERADE
 
 echo "[ PIVPN OK ]"
 
